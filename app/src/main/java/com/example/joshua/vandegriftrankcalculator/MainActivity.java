8bcae33da01f98e740e4bbc4513179b614f06f84
jspspike <jspspike@gmail.com>
2015-09-11 21:50:26 -0500

    Added base files

---
+++ app/src/main/java/com/example/joshua/vandegriftrankcalculator/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.joshua.vandegriftrankcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View v) {
+        EditText curGPA = (EditText) findViewById(R.id.curGPA);
+        EditText desRank = (EditText) findViewById(R.id.desRank);
+        EditText semDone = (EditText) findViewById(R.id.semDone);
+        EditText semLeft = (EditText) findViewById(R.id.semLeft);
+
+    }
+}
